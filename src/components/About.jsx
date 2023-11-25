387b841b7022639f4ef5bf0cb9f9774e7004c33d
iamsandhyamaharjan <98277290+iamsandhyamaharjan@users.noreply.github.com>
2023-11-25 17:47:02 +0545

    in process

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white '>
+          <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                    <div  className='pb-8'>
+                              <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+                    </div>
+                    <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ad in quo enim maxime quas recusandae est, quasi, voluptatum velit, esse dolores commodi natus quis veniam at quidem quia ullam voluptates reiciendis rerum laudantium eveniet! Non, similique! Corrupti possimus repudiandae velit expedita dolorem magni necessitatibus sit ipsa, aliquam distinctio nemo.</p><br/>
+                    <p text-xl>
+                              Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repellat alias a, dolore iste cupiditate cum unde. At esse consequatur, voluptas tempore sed debitis delectus iure, quo fugiat perferendis error doloremque? Placeat officia eius hic qui quisquam, ipsam ducimus nesciunt quam soluta deleniti alias dicta molestiae voluptas aut blanditiis atque.
+                    </p>
+          </div>
+    </div>
+  )
+}
+
+export default About
