1f85e3109f1317793b32c162903c7abd572f77cc
u3sandhya <u3sandhya@get.tinker.com.np>
2025-04-04 15:46:06 +0545

    changed photo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroImage from "../assets/heroImage.png";
+import HeroImage from "../assets/sandhya.PNG";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 
