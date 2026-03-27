87519b8f514df4d230a66c946d772e01c10c1e2c
u3sandhya <u3sandhya@get.tinker.com.np>
2026-03-27 12:13:14 +0545

    updated some links

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,50 +11,50 @@ const Portfolio = () => {
 
   const portfoilios = [
     {
-      id:1,
-      src:arrayDestruct,
-      demo:'https://job123-portal123.netlify.app/',
-      code:'https://github.com/iamsandhyamaharjan/job-portal'
+      id: 1,
+      src: arrayDestruct,
+      demo: 'https://job123-portal123.netlify.app/',
+      code: 'https://github.com/iamsandhyamaharjan/job-portal'
     },
     {
-      id:2,
-      src:reactParallax,
-      demo:'',
-      code:'https://github.com/iamsandhyamaharjan/blood-bank'
+      id: 2,
+      src: reactParallax,
+      demo: '',
+      code: 'https://github.com/iamsandhyamaharjan/blood-bank'
     },
     {
-      id:3,
-      src:restaurant,
-      demo:'https://dinespot.netlify.app',
-      code:'https://github.com/iamsandhyamaharjan/DineSpot'
+      id: 3,
+      src: restaurant,
+      demo: 'https://dinespot.netlify.app',
+      code: 'https://github.com/iamsandhyamaharjan/DineSpot'
     },
 
-   
+
   ]
   return (
     <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
-          <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-                    <div className='pb-8'>
-                              <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
-                              <p className='py-6'>Check out some of my work right here</p>
-                    </div>
-                   
-                    <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-                    {
-                      portfoilios.map(({id,src,demo,code})=>
-                      (    <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
-                      <img src={src} alt='' className='rounded-md duration-200 hover:scale-105'/>
-                      <div className='flex items-center justify-center'>
-                                <button onClick={() => window.open(demo, '_blank')} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-                                <button onClick={() => window.open(code, '_blank')} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
-                      </div>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
+          <p className='py-6'>Check out some of my work right here</p>
+        </div>
+
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 '>
+          {
+            portfoilios.map(({ id, src, demo, code }) =>
+            (<div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+              <img src={src} alt='' className='rounded-md duration-200 hover:scale-105 h-md' />
+              <div className='flex items-center justify-center'>
+                <button onClick={() => window.open(demo, '_blank')} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+                <button onClick={() => window.open(code, '_blank')} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+              </div>
             </div>)
-                        
-                      )
-                    }
-                          
-                    </div>
-          </div>
+
+            )
+          }
+
+        </div>
+      </div>
 
     </div>
   )
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -43,7 +43,7 @@ const SocialLinks = () => {
         Resume <BsFillPersonLinesFill size={30}/>
         </>
       ),
-      href:'/sandhya.pdf',
+      href:'/Sandhya_CV.pdf',
       style:'rounded-tr-md',
       download:true,
     }
